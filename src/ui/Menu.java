43a4ef3e9df4a94e09eeccebd0c974eda852c936
Dominic Grishajev <Dominic@Dominics-MacBook.local>
2017-02-19 15:35:54 +0200

    UI first iteration, fill schedule UI

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,28 +1,65 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import ui.ExamsScheduleTable;
+import ui.TestsScheduleTable;
+import vo.Exam;
 
 
 public class Menu extends JFrame {
 
     private JButton ok;
+    private JButton schedule;
+    private JButton tests;
+    private JButton exams;
+    public static JFrame mainFrm;
 
     public Menu() {
         initialize();
     }
 
     private void initialize() {
-        setTitle("Вхід в систему");
-        JPanel contents = new JPanel();
+        mainFrm = new JFrame("Вхід в систему");
+        mainFrm.getContentPane().setLayout(new FlowLayout());
+        mainFrm.setSize(400, 400);
+        mainFrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ok = new JButton("Вхід");
-        setContentPane(contents);
-        contents.add(ok);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(250, 250);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        schedule = new JButton("Розклад сесії");
+        tests = new JButton("Заліки");
+        exams = new JButton("Іспити");
+        mainFrm.getContentPane().add(ok);
+        mainFrm.getContentPane().add(schedule);
+        mainFrm.setLocationRelativeTo(null);
+        mainFrm.setVisible(true);
+        schedule.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ok.setVisible(false);
+                schedule.setVisible(false);
+                mainFrm.getContentPane().add(tests);
+                mainFrm.getContentPane().add(exams);
+                tests.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        mainFrm.getContentPane().removeAll();
+                        mainFrm.getContentPane().add(tests);
+                        mainFrm.getContentPane().add(exams);
+                        new TestsScheduleTable();
+                    }
+                });
+                exams.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        mainFrm.getContentPane().removeAll();
+                        mainFrm.getContentPane().add(tests);
+                        mainFrm.getContentPane().add(exams);
+                        new ExamsScheduleTable();
+                    }
+                });
+            }
+        });
     }
 
 }
---
+++ src/ui/TestsScheduleTable.java
@@ -0,0 +1,47 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.SwingUtilities;
+
+import ui.Menu;
+
+
+public class TestsScheduleTable extends JFrame {
+
+    DefaultTableModel model = new DefaultTableModel();
+
+    public static JTable jTabSchedule;
+    public static JScrollPane jscrlp;
+
+    public TestsScheduleTable() {
+        jTabSchedule = new JTable(model);
+        model.addColumn("Дисципліна");
+        model.addColumn("Рік навчання");
+        model.addColumn("Дата");
+        model.addColumn("Викладач");
+        model.addColumn("Аудиторія");
+        model.addRow(new Object[] { "ІС", "4", "24.04.2017", "О. П. Жежерун", "1-223" });
+        model.addRow(new Object[] { "УПП", "4", "28.04.2017",  "В. С. Проценко", "1-310" });
+        model.addRow(new Object[] { "ТМ", "4", "20.04.2017", "А. О. Афонін", "2-214" });
+        jscrlp = new JScrollPane(jTabSchedule);
+        jTabSchedule.setPreferredScrollableViewportSize(new Dimension(350, 200));
+        JButton addRow = new JButton("Додати рядок");
+        Menu.mainFrm.getContentPane().add(jscrlp);
+        Menu.mainFrm.getContentPane().add(addRow);
+        Menu.mainFrm.setVisible(true);
+        addRow.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                model.addRow(new Object[] { "Дисципліна", "Рік навчання", "Дата", "Викладач", "Аудиторія" });
+            }
+        });
+    }
+
+}
